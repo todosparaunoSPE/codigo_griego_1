d1500f3ae375bc513068be30c1bf1aab35a6cf8a
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2025-05-14 18:32:04 -0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -135,7 +135,7 @@ else:
 # Mostrar botones de WhatsApp si hay contenido generado
 if 'texto_griego' in st.session_state:
     mensaje1 = f"\n{st.session_state.texto_griego}"
-    mensaje2 = "¿Quieres traducir este código? Ve a:\nhttps://codigo-griego.streamlit.app"
+    mensaje2 = "¿Quieres traducir el código que te ha llegado? Ve a:\nhttps://codigo-griego.streamlit.app"
     enlace1 = crear_enlace_whatsapp(mensaje1)
     enlace2 = crear_enlace_whatsapp(mensaje2)
 
